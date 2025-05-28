42c915d64b16843d9c967c02416018d69c95e9d0
A-Ela <a.eltahir005@gmail.com>
2025-05-28 17:04:39 +0800

    added firebase_options

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 import 'screens/login_screen.dart'; //login screen
 import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
